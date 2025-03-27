b0705f99218c127cd9707e8d66a06cbcd8445ab8
Quin <TheQuinbox@gmail.com>
2025-03-27 07:17:29 -0600

    Single-instance checking.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include "app.hpp"
+#include "single_instance.hpp"
+#include <stdexcept>
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, PSTR, int) {
-	return app{instance}.run();
+	try {
+		single_instance inst("SleepSoundly");
+		return app{instance}.run();
+	} catch (const std::exception& e) {
+		MessageBox(nullptr, e.what(), "Error", MB_OK | MB_ICONERROR);
+		return 1;
+	}
 }
