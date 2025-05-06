9bb44f7bad1daccbc94b08c797f8593efe13009f
Quin <TheQuinbox@gmail.com>
2025-05-06 12:06:53 -0600

    Add script for building with PellesC.

---
+++ build_pelles.bat
@@ -0,0 +1,6 @@
+@echo off
+call povars64.bat
+porc main.rc
+pocc main.c /Ze /Os /std:c23 
+polink /nodefaultlib /entry:main /release /subsystem:windows /out:SleepSoundly.exe main.obj main.res kernel32.lib shell32.lib user32.lib winmm.lib
+pause
