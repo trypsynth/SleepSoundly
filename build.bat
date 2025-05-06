068f4d4c850abb183cdd3fbfd4d0c13c4a93dc6e
Quin <TheQuinbox@gmail.com>
2025-05-06 12:30:05 -0600

    Clean up obj and res files after build.

--- build.bat
+++ build.bat
@@ -3,4 +3,5 @@ call povars64.bat
 porc main.rc
 pocc main.c /Ze /Os /std:c23 
 polink /nodefaultlib /entry:main /release /subsystem:windows /out:SleepSoundly.exe main.obj main.res kernel32.lib shell32.lib user32.lib winmm.lib
+del /s /q *.res *.obj
 pause
